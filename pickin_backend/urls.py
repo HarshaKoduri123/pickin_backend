62db7da3c86acffa7fa742f9c8fe5981c0094063
Harsha <harsha.koduri123@gmail.com>
2024-05-26 01:11:02 -0400

    Updated urls

--- pickin_backend/urls.py
+++ pickin_backend/urls.py
@@ -2,11 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
+from django.urls import re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('pickin_app.urls')),
-    path('', include('pickin_app.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
+    #path('', include('pickin_app.urls')),
 ]
 
 if settings.DEBUG:
